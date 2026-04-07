97a29b1baab2b40a68c7694d4ef4b0193ccaa1c6
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-07 15:17:31 +0400

    add task 3 lessBy10

--- logic2.java
+++ logic2.java
@@ -6,8 +6,12 @@ public class logic2 {
         System.out.println(ex1.fizzString("fib"));*/
 
         // task 2
-        logic2 ex2 = new logic2();
-        System.out.println(ex2.inOrder(1,1,2,true));
+       /* logic2 ex2 = new logic2();
+        System.out.println(ex2.inOrder(1,1,2,true));*/
+
+        // task 3
+        logic2 ex3 = new logic2();
+        System.out.println(ex3.lessBy10(11,1,7));
     }
 
     public String fizzString(String str){
@@ -30,6 +34,13 @@ public class logic2 {
             if(c > b){
                 return true;
             }
+        }
+        return false;
+    }
+
+    public boolean lessBy10(int a, int b, int c){
+        if((Math.abs(a-b) >= 10) || (Math.abs(a-c) >= 10) || (Math.abs(b-c) >= 10)){
+            return true;
         }
         return false;
     }
